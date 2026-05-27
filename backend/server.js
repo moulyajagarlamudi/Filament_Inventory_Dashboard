84402fc92a16259469e4c09ceb81f728127cc0ef
moulyajagarlamudi <122035670+moulyajagarlamudi@users.noreply.github.com>
2026-05-27 15:46:15 +0530

    show google error

--- backend/server.js
+++ backend/server.js
@@ -43,12 +43,10 @@ try {
       const newFilament = await Filament.create(req.body);
       res.json(newFilament);
     } catch (err) {
-      console.error("GOOGLE SHEET ERROR:", err);
+  console.error("GOOGLE SHEET ERROR:", err);
 
-      res.status(500).json({
-        error: err.message,
-    });
-    }
+  res.status(500).json({
+    error: err.message,
   });
 } catch (e) {
   console.warn("Filament model not available to create directly:", e.message);
